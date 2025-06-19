4f615db35ae0ae32ab2c1b78a0fb47f988398db4
ZelalAldag <zelalaldag07@gmail.com>
2025-06-19 21:54:20 +0200

    feat(main): remove scheduled jobs

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-from socket_manager.app import app, socket_app
-from config.settings import MS_HOST, MS_PORT, SESSION_EXPIRATION_TIME
-from events.scheduled_handler import scheduled_thread_read
+from socket_manager.app import socket_app
+from config.settings import MS_HOST, MS_PORT
 from events.handlers import *
-import asyncio
-import schedule
 import uvicorn
-
-
-# Schedule the job every 300 seconds
-schedule.every(SESSION_EXPIRATION_TIME).seconds.do(scheduled_thread_read)
-# schedule.every(30).seconds.do(scheduled_thread_read) # For testing purposes
-
-
-# Async function to run schedule loop
-async def run_scheduler():
-    while True:
-        schedule.run_pending()
-        await asyncio.sleep(30)  # Check every second for pending tasks
-
-
-# Start scheduler on FastAPI startup
-@app.on_event("startup")
-async def start_scheduler():
-    asyncio.create_task(run_scheduler())  # Runs in the background
 
 
 if __name__ == "__main__":
